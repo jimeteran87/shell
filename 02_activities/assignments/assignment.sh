7edfb017090cc9dba88667b8f3019adbd7b131fb
jimeteran <jimeteran@gmail.com>
2025-04-21 17:20:46 -0400

    Changes

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -48,4 +48,4 @@ rm -f data/raw/*ipaddr*
 # 8. Create a file named ./data/inventory.txt listing all files in ./data/processed
 find data/processed -type f > data/inventory.txt
 
-echo "Project setup is complete!!!"
+echo "Project setup is complete!!!!"
